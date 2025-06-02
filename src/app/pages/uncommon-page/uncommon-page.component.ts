1e0b3543a8bd2ca701dee548d357b3f9ae8c258b
Victor Garcia <victor.garcia.florez@gmail.com>
2025-06-02 12:17:25 +0200

    Update example of use i18SelectPipe.

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Fernando',
+  gender: 'male',
+  age: 39,
+  address: 'Ottawa, Canada'
+};
+
+const client2 = {
+  name: 'Melissa',
+  gender: 'female',
+  age: 32,
+  address: 'Toronto, Canada'
+};
+
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent],
+  imports: [CardComponent, I18nSelectPipe],
   templateUrl: './uncommon-page.component.html'
 })
 // Es default porque se utilizará por defecto eventualmente. "Para poder importar facilmente de las rutas padre"
-export default class UncommonPageComponent { }
+export default class UncommonPageComponent {
+  // i18n Select
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
+
+  // toggle
+  changeClient() {
+    if(this.client() === client1) {
+      this.client.set(client2);
+      return;
+    }
+
+    this.client.set(client1);
+  }
+}
