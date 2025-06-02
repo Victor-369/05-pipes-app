8ef01ac20dbb952b036d93338caf91a64758f5dd
Victor Garcia <victor.garcia.florez@gmail.com>
2025-06-02 12:44:18 +0200

    Added example for i18PluralPipe

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
-import { I18nSelectPipe } from '@angular/common';
+import { I18nPluralPipe, I18nSelectPipe } from '@angular/common';
 
 const client1 = {
   name: 'Fernando',
@@ -19,12 +19,13 @@ const client2 = {
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent, I18nSelectPipe],
+  imports: [CardComponent, I18nSelectPipe, I18nPluralPipe],
   templateUrl: './uncommon-page.component.html'
 })
 // Es default porque se utilizará por defecto eventualmente. "Para poder importar facilmente de las rutas padre"
 export default class UncommonPageComponent {
-  // i18n Select
+
+  /************ i18n Select ************/
   client = signal(client1);
 
   invitationMap = {
@@ -41,4 +42,27 @@ export default class UncommonPageComponent {
 
     this.client.set(client1);
   }
+
+
+  /************ i18n Plural ************/
+  clientsMap = signal({
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    other: 'tenemos # clientes esperando'
+  });
+
+  clients = signal([
+    'Maria',
+    'Pedro',
+    'Fernando',
+    'Melissa',
+    'Natalia',
+    'Andrea',
+    'Juan',
+    'Carlos',
+  ]);
+
+  deleteClient() {
+    this.clients.update((prev) => prev.slice(1));
+  };
 }
